aa5d43813640456ec1602d7b035a282d51c001d7
build-with-paradox <contact@buildwithparadox.com>
2025-05-18 08:39:29 +0530

    next.config.ts file correction

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["m.media-amazon.com", "res.cloudinary.com"], // Add Cloudinary domain
+    domains: ["m.media-amazon.com", "res.cloudinary.com"],
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
